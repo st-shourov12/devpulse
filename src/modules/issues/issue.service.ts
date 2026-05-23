b0f3890aaad0134236f18d48eeebd5992f5ff8db
st-shourov12 <shourovsc38@gmail.com>
2026-05-23 23:35:03 +0600

    change update issue

--- src/modules/issues/issue.service.ts
+++ src/modules/issues/issue.service.ts
@@ -174,7 +174,7 @@ const updateIssueFromDB = async (id: string, req: Request, res: Response) => {
             title=COALESCE($1,title), 
             description=COALESCE($2,description),
             type= COALESCE($3,type),
-            status=COALESCE($4,status),
+            status=COALESCE($4, 'in_progress'),
             updated_at= NOW()
             WHERE  id=$5
             RETURNING *
@@ -193,7 +193,7 @@ const updateIssueFromDB = async (id: string, req: Request, res: Response) => {
             title=COALESCE($1,title), 
             description=COALESCE($2,description),
             type= COALESCE($3,type),
-            status=COALESCE($4,status),
+            status=COALESCE($4, 'in_progress'),
             updated_at= NOW()
             WHERE  id=$5
             RETURNING *
